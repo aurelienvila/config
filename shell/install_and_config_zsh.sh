f92a23cf2921ee2c2c95ef934bd95ae03dc91c77
Aurelien Vila <aurelien.vila@live.fr>
2021-02-13 10:50:42 +0100

    add shell install script

---
+++ shell/install_and_config_zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DIR="$(dirname "$(readlink -f "$0")")" &&
+ZSHRC=$DIR/.zshrc &&
+echo $ZSHRC
+
+sudo apt install curl zsh fonts-powerline -y &&
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+
+rm $HOME/.zshrc &&
+ln -s $ZSHRC $HOME 
